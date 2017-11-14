c48f525b927c746439e2f1c53b4e6e33ddad0623
Dan Logajan <dlogajan@gmail.com>
2017-11-13 19:04:23 -0500

    add functioning reset button

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
+
+  let $resetButton = $('#reset');
   let $gridItems;
   let $grid = $('#grid');
   let $gridItem = $('<div></div>', {'class': 'grid-item'});
+
+  $resetButton.on('click', resetButton);
+
+  function resetButton(e) {
+    e.preventDefault();
+    let userInput = prompt('Please enter a grid width (1-100):');
+    createGrid(userInput);
+  }
 
   function createGrid(width) {
     let totalGridItems = width * width;
@@ -13,12 +23,24 @@ $(document).ready(function(){
       $gridItem.clone().appendTo($grid);
     }
     $gridItems = $('.grid-item');
+    assignHandler($gridItems);
+    removeClass($gridItems, 'black');
   }
-  
-  createGrid(40);
 
-  $gridItems.on('mouseenter', function() {
-    $(this).addClass('black');
-  });
+  function assignHandler(els) {
+    els.on('mouseenter', function() {
+      $(this).addClass('black');
+    });
+  }
+
+  function removeClass(els, cls) {
+    els.each(function() {
+      if ($(this).hasClass(cls)) {
+        $(this).removeClass(cls);
+      }
+    });
+  }
+
+  createGrid(40);
 
 });
